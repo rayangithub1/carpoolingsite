c325686a31d1a5506567ead3e6793831645d8490
rayangithub1 <rayanasif1080@gmail.com>
2026-06-23 06:00:21 +0500

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import {
   Shield, Users, CheckCircle, ArrowRight, Eye, EyeOff,
